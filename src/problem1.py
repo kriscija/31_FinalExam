bb0ab0bbc9bc280547801ae3aa4ce223056c2001
kriscija <kriscija@rose-hulman.edu>
2018-05-24 14:25:52 -0400

    final

--- src/problem1.py
+++ src/problem1.py
@@ -19,8 +19,8 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    run_test_problem1a()
-    # run_test_problem1b()
+    #run_test_problem1a()
+    #run_test_problem1b()
 
 
 def run_test_problem1a():
@@ -157,7 +157,12 @@ def run_test_problem1b():
 
 
 def problem1b(numbers):
+    sum = 0
     initia = len(numbers)//3
+    for k in range(initia,initia*2):
+        sum = sum + numbers[k]
+    return sum
+
 
 
     """
@@ -178,7 +183,7 @@ def problem1b(numbers):
       :type numbers:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement and test this function.
+    # DoneTODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
 
